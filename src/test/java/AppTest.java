a8ad09b97d84486fc467d8e0b4c4dd1f19128f6d
habibisrafilov <habib84@mail.ru>
2019-07-30 22:47:50 -0400

    change class

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -3,5 +3,7 @@ public class AppTest {
         System.out.println("Hello world");
         System.out.println("Hello world");
         System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
     }
 }
