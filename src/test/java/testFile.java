5f92e199bba3f9f0ec730316d5dcd6dad99c4db6
habibisrafilov <habib84@mail.ru>
2019-07-30 23:02:00 -0400

    new file

---
+++ src/test/java/testFile.java
@@ -0,0 +1,8 @@
+public class testFile {
+    public static void main(String[] args) {
+        System.out.println("Java");
+        System.out.println("Java");
+        System.out.println("Java");
+
+    }
+}
