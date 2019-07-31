c451763c3174dd7700e66adcf7d1c7beb0e1983b
habibisrafilov <habib84@mail.ru>
2019-07-30 22:44:22 -0400

    initial commit

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,7 @@
+public class AppTest {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+    }
+}
